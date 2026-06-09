50a8d3b6df7adb288325d5ab0ad485ef0496f6e7
amathguywhocodes <nbastepe03@gmail.com>
2026-06-09 18:04:43 +0300

    2nd commit

--- hello.py
+++ hello.py
@@ -1,17 +1,25 @@
 
 # save this as hello.py
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+print(random.__name__)
 print(__name__)
 
 
-# @app.route("/")
-# def hello():
-#     return "Hello, World!"
+@app.route("/")   # "/" says go to homepage
+def hello():
+    return "Hello, World!"
 
-if __name__ == '__main__':
+@app.route("/bye")
+def bye():
+    return "Bye!"
+
+#if the __name__ belongs to the file that's being run (it's not imported)
+if __name__ == '__main__': # __main__ denotes the file that's being run
+    # does the same thing we did on Terminal (by writing
+    # "export FLASK_APP=hello.py" and "flask run" to the Terminal)
     app.run()
 
-    
